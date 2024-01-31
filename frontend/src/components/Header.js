ef494422ade12c2a113b5e54c4d42f40e8c872e6
Gops 'eh <gopala200218@gmail.com>
2024-01-31 15:49:26 +0530

    header merge

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,41 @@
-import { Box, Flex, Image, Spacer, Input } from '@chakra-ui/react';
+import { Box, Flex, Image, Spacer, Input, Text, HStack, Button} from '@chakra-ui/react';
 import React from 'react';
-import logo from '../images/glamazon-logo.png'
+import logo from '../images/glamazon-logo.png';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function Header() {
   return (
     <div>
-      <Flex px={4} border='1px'>
+      <Flex px={4} border="1px">
         <Box pt="2">
-          <Image w={180} h={20} src={logo}/>
+          <Image w={180} h={10} src={logo} />
         </Box>
-        <Spacer/>
-        <Box p="4">
-        <Input size='lg' placeholder='Basic usage' />
+        <Spacer />
+        <Box
+          px={3}
+          pt={2}
+          w={{ base: "150px", md: "300px", lg: "420px", xl: "700px" }}
+          minW="150px"
+        >
+          <Input
+            size="sm"
+            py={4}
+            px={7}
+            border="1px solid rgba(0, 0, 66, 0.5)"
+            borderRadius={5}
+            placeholder="What are you looking for?"
+          />
         </Box>
-        <Spacer/>
-        <Box p="4" bg="green.400">
-          Box 2
+        <Spacer />
+        <Box w={{ base: "350px", md: "370px", lg: "370px", xl: "370px" }}>
+          <HStack justifyContent='center' pt={1} spacing={6}>
+            <Text>Sign in & Sign Up</Text>
+            <HStack>
+              <FavoriteIcon style={{ color: "#663620", fontSize: "18px" }} />
+              <Text>Wishlist</Text>
+            </HStack>
+            <Button color='#663620' _hover={{ bg: '#663620', color:'white', transition:'0.4s' }} variant='ghost'>3D TRY ON</Button>
+          </HStack>
         </Box>
       </Flex>
     </div>
