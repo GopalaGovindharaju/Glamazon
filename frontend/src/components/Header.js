9d9b2793f0b7dc3dc6761699add4ee241494e84f
Gops 'eh <gopala200218@gmail.com>
2024-01-31 12:50:38 +0530

    started

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import { Box, Flex, Image, Spacer, Input } from '@chakra-ui/react';
+import React from 'react';
+import logo from '../images/glamazon-logo.png'
+
+function Header() {
+  return (
+    <div>
+      <Flex px={4} border='1px'>
+        <Box pt="2">
+          <Image w={180} h={20} src={logo}/>
+        </Box>
+        <Spacer/>
+        <Box p="4">
+        <Input size='lg' placeholder='Basic usage' />
+        </Box>
+        <Spacer/>
+        <Box p="4" bg="green.400">
+          Box 2
+        </Box>
+      </Flex>
+    </div>
+  );
+}
+
+export default Header
