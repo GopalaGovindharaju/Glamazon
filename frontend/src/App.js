4e429bd5f964153a2f240156792a326b881ad5fe
Abishake Dharmaraj <abishake.dr@gmail.com>
2024-02-02 20:21:49 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import Second from './components/second';
 
 
+
 function App(props) {
   return (
     <div className="App">
       <Header/>
       <Second/>
+     
     </div>
   );
 }
