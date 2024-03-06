72002fc7f627abdb744c6d6a165c83f37f5c76e6
Gops 'eh <gopala200218@gmail.com>
2024-03-06 15:14:27 +0530

    djfjd

--- frontend/src/labcomponents/StyledItems.js
+++ frontend/src/labcomponents/StyledItems.js
@@ -85,11 +85,25 @@ function StyledItems({snapedImage}) {
       border="1px"
       overflow="auto"
       height='40vh'
+      width='100%'
+      flexDirection='row'
       pl='17%'
-      justifyContent="space-evenly"
-      flexWrap="wrap"
     >
-
+{Array.from({ length: 10 }).map((_, index) => (
+        <Card key={index} h='auto' minW="270px" my={2} mr={4}>
+          <CardBody display="flex" pb={0} flexDirection="column">
+            <Image
+              h="100%"
+              src={snapedImage}
+              borderRadius="lg"
+              loading="lazy"
+            />
+            <Stack mt="2" spacing="3">
+              <Heading size="md">sample</Heading>
+            </Stack>
+          </CardBody>
+        </Card>
+      ))} 
     </Box>
     </>
   );
