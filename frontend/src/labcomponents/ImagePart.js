cf7df5b70de69eacd58ade0aed89e49b26bdf99e
chitra lakshmi <chitraraveendran2002@gmail.com>
2024-03-19 14:32:46 +0530

    pull

--- frontend/src/labcomponents/ImagePart.js
+++ frontend/src/labcomponents/ImagePart.js
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, Image, Spinner, Stack, Switch, Text } from '@chakra-ui/react'
+import { Box, Button, CardBody, Center, Heading, Image, Spinner, Stack, Switch, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useSnapImage } from '../context/SnapImageContext';
 import { Hairstyle } from '../components/Hairstyle';
@@ -12,104 +12,125 @@ function ImagePart({selectedHaircolor,selectedHairStyle,setShownImage,shownImage
     setShownImage(e.target.checked);
     console.log(e.target.checked);
   }
+
+  const text_css = {
+    fontFamily: "var(--chakra-fonts-body)",
+    fontWeight:'400',
+    color: "rgb(51, 51, 51)",
+    fontSize: "18px",
+  };
+
   return (
     <div>
       <Box
         pt="6.5%"
         pl="17%"
         display="flex"
-        border="1px"
         height="calc(100vh - 40vh)"
         overflow="auto"
         flexWrap="wrap"
         alignContent="stretch"
+        backgroundColor="#FAF2EC"
       >
         {/*<Box  className='col-6' border='1px solid black' borderRadius="9" mb={2} backgroundColor='rgb()'>
       <Image pt={2} objectFit='contain' w='100%' height="calc(100vh - 55vh)" src={snapedImage} alt='Dan Abramov' />
   </Box>*/}
         <Box
           className="col-6"
-          border="1px solid black"
+          boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
           borderRadius="9"
-          mb={2}
-          backgroundColor="rgb()"
+          width="42%"
+          margin="8px"
+          marginLeft="3%"
+          backgroundColor="white"
         >
-          <Stack direction="row" pt={2} pl={2}>
-            <Text>OriginalImage</Text>
+          <Stack direction="row" justify="center" pt={2} pl={2}>
+            <Text style={text_css}>ORIGINAL IMAGE</Text>
             <Switch
               pt={1}
               colorScheme="yellow"
               isChecked={shownImage}
               onChange={handleImageSwitch}
             ></Switch>
-            <Text>FilteredImage</Text>
+            <Text style={text_css}>FILTERED IMAGE</Text>
           </Stack>
           {loading ? (
             <Spinner
               thickness="4px"
               speed="0.65s"
               emptyColor="gray.200"
-              color="blue.500"
+              color="orange.700"
               size="xl"
             />
           ) : shownImage ? (
-            <Image
-              pt={2}
-              objectFit="contain"
-              w="100%"
-              height="calc(100vh - 63vh)"
-              src={filteredImage}
-              alt="Dan Abramov"
-            />
+            <Stack direction="row" justify="center"><Image
+            pt={2}
+            objectFit="fit"
+            w="40%"
+            height="calc(100vh - 63vh)"
+            src={filteredImage}
+            alt="Dan Abramov"
+            borderRadius="md"
+            boxSize='200px'
+            boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
+          /></Stack>
+            
           ) : (
-            <Image
-              pt={2}
-              objectFit="contain"
-              w="100%"
-              height="calc(100vh - 63vh)"
-              src={snapedImage}
-              alt="Dan Abramov"
-            />
+            <Stack direction="row" justify="center">
+              <Image
+                objectFit="fit"
+                w="40%"
+                height="calc(100vh - 65vh)"
+                src={snapedImage}
+                alt="Dan Abramov"
+                borderRadius='md'
+                boxSize='200px'
+                boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
+              />
+            </Stack>
           )}
         </Box>
         <Box
           className="col-6"
-          border="1px solid black"
+          boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
           borderRadius="9"
-          mb={2}
-          backgroundColor="rgb()"
+          margin="8px"
+          backgroundColor="white"
         >
-          <Text>Applied Styles</Text>
+          <Stack direction="row" justify="center" pt={2}><Text style={text_css}>APPLIED STYLES</Text></Stack>
           <Stack direction="row">
-            <Stack direction="column" mx={3}>
-              <Text mb={0}>Hairstyle</Text>
+            <Stack direction="column" width="35%" mx={3}>
+              <Text style={text_css} mb={0}>Hairstyle</Text>
               {hairstyle.image && (
                 <Image
-                  pt={2}
                   objectFit="fit"
                   w="100%"
                   height="calc(100vh - 68vh)"
                   src={hairstyle.image}
                   alt="Dan Abramov"
+                  borderRadius="md"
+                  boxSize='180px'
+                  boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
                 />
               )}
             </Stack>
-            <Stack direction="column">
-              <Text mb={0}>HairColor</Text>
+            <Stack direction="column" width="36%">
+              <Text style={text_css} mb={0}>HairColor</Text>
               <Box
-                width="200px"
-                height="calc(100vh - 69vh)"
+                height="calc(100vh - 68vh)"
                 variant="solid"
-                mt={2}
                 mr={2}
                 backgroundColor={selectedHaircolor}
                 borderColor="black"
-                border="1px"
+                border="0.6px"
+                borderRadius="md"
+                boxSize='178px'
+                boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
               />
             </Stack>
             <Stack direction="column">
-              <Text mb={0}>Catogory</Text>
-              <Text>{hairstyle.category ? hairstyle.category : ""}</Text>
+              <Text style={text_css} mb={0}>Catogory</Text>
+              <Text fontFamily="LKFuturaStd-Medium" fontWeight="bold">{hairstyle.category ? hairstyle.category : ""}</Text>
             </Stack>
           </Stack>
         </Box>
