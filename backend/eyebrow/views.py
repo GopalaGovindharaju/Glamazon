1bbb81e0c7d98ddcaca2a97c065483a5f0552fb5
chitra lakshmi <chitraraveendran2002@gmail.com>
2024-03-23 16:48:06 +0530

    pull

--- backend/eyebrow/views.py
+++ backend/eyebrow/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from openai import OpenAI
 import cv2
 import numpy as np
 import requests
@@ -19,10 +20,11 @@ def getFilteredEyebrow(request):
             modified_image = fetch_and_remove_eyebrows(Original_Image)
 
             if modified_image is not None:
+                main(Original_Image, modified_image, prompt)
                 # Save the modified image to a specific path
-                modified_image_path = 'C:/Users/WELCOME/Downloads/mask_image.png'  # Specify the desired path here
-                cv2.imwrite(modified_image_path, modified_image)
-                print("gr",type(modified_image))
+                #modified_image_path = 'C:/Users/WELCOME/Downloads/mask_image.png'  # Specify the desired path here
+                #cv2.imwrite(modified_image_path, modified_image)
+                #print("gr",type(modified_image))
 
                 # Return the path to the modified image
                 return Response({'message': 'Image processed successfully', 'modified_image_path': modified_image_path})
@@ -32,6 +34,24 @@ def getFilteredEyebrow(request):
         else:
             return Response({'error': 'No image file provided'})
 
+def main(image, mask, prompt):
+    api_key = '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'
+    client = OpenAI(api_key = api_key)
+
+    image_data = image.read()
+    _, mask_data = cv2.imencode('.png', mask)
+    mask1 = mask_data.tobytes()
+
+    response = client.images.edit(
+    image=image_data,
+    mask=mask1,
+    prompt="A girl with green hairstyle",
+    n=2,
+    size="1024x1024"
+    )
+
+    print(response)
+
 
 def fetch_and_remove_eyebrows(image_file):
     # Define the API endpoint
@@ -40,7 +60,7 @@ def fetch_and_remove_eyebrows(image_file):
     payload = {'max_face_num': 1, 'face_field': 'age,gender,landmark72'}
     image_data = image_file.read()  # Read the image data once
     files = [('image', ('file', image_data, 'application/octet-stream'))]
-    headers = {'ailabapi-api-key': 'fw6CZvUBXzuym8EwXx4TLtAKBHPOMLce1lfRSWKpQ1W0CY52qcTvFugoNRDAnteb'}  # Assuming api_key is defined somewhere
+    headers = {'ailabapi-api-key': 'fH0mMe7AReJO65UdigJR01oi9FDcExpL3yKIqa2LqQPjYwWnGYryuVhWM9pSKflG'}  # Assuming api_key is defined somewhere
 
     # Make API request
     response = requests.post(url, headers=headers, data=payload, files=files)
