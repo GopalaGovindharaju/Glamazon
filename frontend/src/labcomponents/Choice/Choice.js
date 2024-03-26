8c505faddac35d6f872a8afaa84012f1dec2ae28
gopi <gopivelraj7169@gmail.com>
2024-03-26 15:50:15 +0530

    pull

--- frontend/src/labcomponents/Choice/Choice.js
+++ frontend/src/labcomponents/Choice/Choice.js
@@ -1,22 +1,62 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image, Spinner, Stack, Switch, Text } from '@chakra-ui/react'
 import React from 'react'
 
 
 function Choice() {
+  const text_css = {
+    fontFamily: "Cambria",
+    fontWeight:'550',
+    color: "rgb(51, 51, 51)",
+    fontSize: "18px",
+  };
   return (
-    <Box
+
+<>
+   <Box
     position="fixed"
     left={0}
     bottom={0}
     top="14%"
-    right="83%"
+    right="75%"
     py={4}
     px={4}
     bg='rgb(251, 249, 247)'
     zIndex={9}
   >
-    Choice
-  </Box>
+  
+          <Stack direction="column" justify="center" pt={2}><Text style={text_css}>APPLIED STYLES</Text></Stack>
+          <Stack direction="column" mt={8} >
+            <Stack direction="row" w="55%">
+              <Text style={text_css} mb={0} py={4}>Hairstyle</Text>
+                <Text fontFamily="LKFuturaStd-Medium">{}</Text>
+          
+            </Stack>
+            <Stack direction="row" w="33%">
+              <Text style={text_css} mb={0} py={4}>HairColor</Text>
+              <Text fontFamily="LKFuturaStd-Medium">{}</Text>
+            </Stack>
+            <Stack direction="row" w="33%">
+              <Text style={text_css} mb={0} py={4}>Catogory</Text>
+              <Text fontFamily="LKFuturaStd-Medium">{}</Text>
+            </Stack>
+            <Stack direction="row" w="33%">
+              <Text style={text_css} mb={0} py={4}>LipColor</Text>
+              <Text fontFamily="LKFuturaStd-Medium">{}</Text>
+            </Stack>
+            <Stack direction="row" w="33%">
+              <Text style={text_css} mb={0} py={4}>EyeBrow</Text>
+              <Text fontFamily="LKFuturaStd-Medium">{}</Text>
+            </Stack>
+          </Stack>
+        
+        </Box>
+       
+       
+
+ </>
+ 
+  
+
   )
 }
 
---
+++ frontend/src/labcomponents/Choice/ChoiceImagePart.js
@@ -0,0 +1,84 @@
+import { Box, Image, Spinner, Stack, Switch, Text } from '@chakra-ui/react'
+import React from 'react'
+import { useSnapImage } from '../../context/SnapImageContext';
+
+
+function ChoiceImagePart({setShownImage,shownImage,loading}) {
+    const { filteredImage } = useSnapImage();
+    const { snapedImage } = useSnapImage();
+    const handleImageSwitch = (e) => {
+        setShownImage(e.target.checked);
+        console.log(e.target.checked);
+      }
+    const text_css = {
+        fontFamily: "Cambria",
+        fontWeight:'550',
+        color: "rgb(51, 51, 51)",
+        fontSize: "18px",
+      };
+  return (
+    <div>
+       <Box
+        pt="6rem"
+        pl="17%"
+        display="flex"
+        height="calc(100vh)"
+        overflow="auto"
+        flexWrap="wrap"
+        alignContent="stretch"
+        backgroundColor="#FAF2EC"
+      >
+        <Box
+          borderRadius="9"
+          width="58%"
+          backgroundColor="#FAF2EC"
+        >
+          <Stack direction="row" justify='center'  pt={2}>
+            <Text style={text_css}>ORIGINAL IMAGE</Text>
+            <Switch
+              pt={1}
+              colorScheme="yellow"
+              isChecked={shownImage}
+              onChange={handleImageSwitch}
+            ></Switch>
+            <Text style={text_css}>FILTERED IMAGE</Text>
+          </Stack>
+          {loading ? (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="orange.700"
+              size="xl"
+            />
+          ) : shownImage ? (
+            <Stack direction="row" justify='center'>
+            <Image
+            objectFit="fit"
+            height="calc(100vh - 22vh)"
+            src={filteredImage}
+            alt="Dan Abramov"
+            borderRadius="md"
+            boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
+          /></Stack>
+            
+          ) : (
+            <Stack direction="row" justify='center'>
+              <Image
+                objectFit="fit"
+                height="calc(100vh - 22vh)"
+                src={snapedImage}
+                alt="Dan Abramov"
+                borderRadius='md'
+                boxShadow='0 0 5px rgba(60, 60, 60, 0.5)'
+              />
+            </Stack>
+          )}
+        </Box>
+        
+      </Box>
+    </div>
+  )
+}
+
+export default ChoiceImagePart
